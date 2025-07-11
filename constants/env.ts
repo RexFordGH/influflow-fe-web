3ac7dcf151ecb3caf4f328eae6b6e7adeda7d257
Rex <lguangcong163@gmail.com>
2025-07-11 20:24:05 +0800

    feat: prepare test environment

--- constants/env.ts
+++ constants/env.ts
@@ -1,4 +1,7 @@
-export const API_BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? '/api/proxy' // 开发环境使用手动代理路由
-    : 'https://influflow-api.up.railway.app'; // 生产环境直接请求
+export const isLocal = process.env.NEXT_PUBLIC_ENV === 'local';
+export const isDev = process.env.NEXT_PUBLIC_ENV === 'test';
+export const isProd = process.env.NEXT_PUBLIC_ENV === 'production';
+
+export const API_HOST = process.env.NEXT_PUBLIC_API_BASE_URL; // 测试/生产环境 ->直接请求后端环境
+// 开发环境 -> next api 代理路由
+export const API_BASE_URL = isLocal ? '/api/proxy' : API_HOST;
