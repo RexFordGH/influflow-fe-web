2702af87fbf45ea7f0960b3f7952894b8dd45e7a
Rex <lguangcong163@gmail.com>
2025-08-05 15:39:29 +0800

    feat: set rounded-none to twitter card

--- components/trending/TwitterCard.tsx
+++ components/trending/TwitterCard.tsx
@@ -119,7 +119,12 @@ export function TwitterCard({ html, className = '' }: TwitterCardProps) {
       ></div>
 
       <div className="absolute top-[12px] left-[1px] rounded-t-[8px] w-[288px] h-[56px] bg-white overflow-hidden">
-        <Image src="/images/twitter.png" width={288} height={56} />
+        <Image
+          src="/images/twitter.png"
+          width={288}
+          height={56}
+          className="rounded-none"
+        />
       </div>
     </div>
   );
