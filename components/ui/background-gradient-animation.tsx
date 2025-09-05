75ded15cb145e55e86fda9d7316b47f1541f1d40
Draco <yanlong2944@gmail.com>
2025-09-05 18:20:23 +0800

    feat: Op homepage animation

--- components/ui/background-gradient-animation.tsx
+++ components/ui/background-gradient-animation.tsx
@@ -10,7 +10,7 @@ export const BackgroundGradientAnimation = ({
   thirdColor = '255, 153, 156',
   fourthColor = '255, 153, 156',
   fifthColor = '255, 160, 64',
-  pointerColor = '68, 138, 255',
+  pointerColor = '255, 153, 156',
   size = '55%',
   blendingValue = 'hard-light',
   children,
@@ -188,25 +188,25 @@ export const BackgroundGradientAnimation = ({
         <div
           className={cn(
             `absolute [background:radial-gradient(circle_at_center,_rgba(var(--third-color),_0.8)_0,_rgba(var(--third-color),_0)_50%)_no-repeat]`,
-            `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
+            `[mix-blend-mode:var(--blending-value)] w-[450px] h-[450px] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
             `[transform-origin:calc(40%-70px)]`,
             `animate-third`,
             `opacity-100`,
           )}
-        ></div>
-        <div
+        ></div> 
+        {/* <div
           className={cn(
             `absolute [background:radial-gradient(circle_at_center,_rgba(var(--fourth-color),_0.8)_0,_rgba(var(--fourth-color),_0)_50%)_no-repeat]`,
-            `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
+            `[mix-blend-mode:var(--blending-value)] w-[250px] h-[250px]  top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
             `[transform-origin:calc(60%+90px)]`,
             `animate-fourth`,
             `opacity-70`,
           )}
-        ></div>
+        ></div> */}
         <div
           className={cn(
             `absolute [background:radial-gradient(circle_at_center,_rgba(var(--fifth-color),_0.8)_0,_rgba(var(--fifth-color),_0)_50%)_no-repeat]`,
-            `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
+            `[mix-blend-mode:var(--blending-value)] w-[250px] h-[250px] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,
             `[transform-origin:calc(50%-50px)_calc(50%+50px)]`,
             `animate-fifth`,
             `opacity-100`,
@@ -218,7 +218,7 @@ export const BackgroundGradientAnimation = ({
             ref={interactiveRef}
             className={cn(
               `absolute [background:radial-gradient(circle_at_center,_rgba(var(--pointer-color),_0.8)_0,_rgba(var(--pointer-color),_0)_50%)_no-repeat]`,
-              `[mix-blend-mode:var(--blending-value)]  w-[var(--size)] h-[var(--size)]`,
+              `[mix-blend-mode:var(--blending-value)]  w-[450px] h-[450px]`,
               `opacity-70`,
             )}
             style={{ zIndex: 1 }}
